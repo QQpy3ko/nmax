a1821864d8ea03a957acfbf4994f5ccc1750c952
Aleksandr Vinogradov <QQpy3ko@gmail.com>
2020-05-23 15:53:20 +0300

    add logic to nmax lib, script running

---
+++ bin/nmax.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+require "../lib/nmax"
+
+Lib::NmaxRunner.run(ARGV[0].to_i)
--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,3 +1,20 @@
 require_relative "./nmax/version"
 
-class Error < StandardError; end
+module Lib
+  class Error < StandardError; end
+
+  class NmaxRunner
+    class << self
+      def run(n)
+        int_array = []
+        STDIN.each do |line|
+          line.chomp.scan(/\d+/) do | int_subsequence |
+            int_array << int_subsequence.to_i
+          end
+        end
+        puts int_array.max(n)
+      end
+    end
+  end
+end
+
